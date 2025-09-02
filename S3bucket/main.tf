5e114742c71650234ec8c0cfcbc3705fbd22b213
Msdrushi07 <108717120+Msdrushi07@users.noreply.github.com>
2025-09-02 17:52:42 +0530

    Update main.tf

--- S3bucket/main.tf
+++ S3bucket/main.tf
@@ -1,1 +1,15 @@
+# Define AWS provider
+provider "aws" {
+  region = "us-east-1"
+}
 
+# Create S3 bucket
+resource "aws_s3_bucket" "demo_bucket" {
+  bucket = "rushi-terraform-demo-bucket-12345"  # must be globally unique
+  acl    = "private"
+
+  tags = {
+    Name        = "TerraformDemoBucket"
+    Environment = "Dev"
+  }
+}
